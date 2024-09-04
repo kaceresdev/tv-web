ff7a9b5b0b967b20d2a2ae704b7aacfb867423a5
Javier Cáceres Bordallo <kaceresdev@gmail.com>
2024-09-04 20:08:42 +0200

    V1 aplication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,62 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ActivatedRoute, RouterOutlet } from "@angular/router";
+import { environment } from "../environments/environment";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  imports: [CommonModule, RouterOutlet, FormsModule],
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'tv-web';
+export class AppComponent implements OnInit {
+  mobile_number = "";
+  name = "";
+  name_client = "";
+  mobile_client = "";
+  step = 1;
+  numberGenerated = 0;
+  mostrarMensaje: boolean = false;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.name = params["name"];
+      if (this.name === "javi") {
+        this.mobile_number = environment.numberJavi;
+      } else if (this.name === "adri") {
+        this.mobile_number = environment.numberAdri;
+      }
+    });
+  }
+
+  randomNumberGen() {
+    const numeroAleatorio = Math.floor(100000 + Math.random() * 900000);
+    const timestamp = Date.now();
+    const timestampUltimos6Digitos = timestamp % 1000000;
+    const numeroUnico = (numeroAleatorio + timestampUltimos6Digitos) % 1000000;
+    this.step++;
+    this.numberGenerated = numeroUnico < 100000 ? numeroUnico + 100000 : numeroUnico;
+  }
+
+  copyText(text: number): void {
+    navigator.clipboard
+      .writeText(text.toString())
+      .then(() => {
+        this.mostrarMensaje = true;
+        setTimeout(() => {
+          this.mostrarMensaje = false;
+        }, 3000);
+      })
+      .catch((err) => {
+        console.error("Error al copiar el texto: ", err);
+      });
+  }
+
+  nextStep() {
+    this.step++;
+  }
 }
